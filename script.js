998ca69c1ad98af2718954f939192e443a7f8b14
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-12-08 17:16:23 -0800

    Retrying script

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let currentNumber = ''; // current number button pressed
-let previousNumber = ''; // previous operation result
+let firstOp = ''; // current number button pressed
+let secondOp= ''; // previous operation result
 let operator = null; // operators
-let shouldReset = false; 
-let shouldOperate = false;
-let maxDecimal = false;
+let decimalCount = 0;
 
 const currentNumberDisplay = document.querySelector('.current-number');
 
@@ -17,9 +15,13 @@ const equalsButton = document.querySelector('.equals-button');
 window.addEventListener('keydown', handleKeyboardInput);
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        handleNumber(button.textContent);
+        appendNumber(button.textContent);
     });
 });
+
+decimalButton.addEventListener('click', () => {
+    appendDecimal();
+})
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -39,92 +41,21 @@ equalsButton.addEventListener('click', () => {
     handleEqual();
 })
 
-decimalButton.addEventListener('click', () => {
-    handleDecimal();
-})
-
-function clear() {
-    currentNumber = '';
-    previousNumber = '';
-    operator = '';
-    shouldReset = false;
-    shouldOperate = false;
-    maxDecimal = false;
-    currentNumberDisplay.textContent = '';
+function appendNumber(number) {
+    currentNumberDisplay.textContent += number;
 }
 
-function del(number) {
-    currentNumber = number.slice(0, -1);
-    currentNumberDisplay.textContent = currentNumber;
-}
-
-function handleNumber(number) {
-    if (shouldReset) {
-        currentNumber = '';
-        shouldReset = false;
-        shouldOperate = true;
-    }
-    currentNumber += number;
-    currentNumberDisplay.textContent = currentNumber;
-}
-
-function handleOperator(op) {
-    if (shouldOperate) {
-        previousNumber = operate();
-        currentNumberDisplay.textContent = previousNumber;
-        operator = op;
-        maxDecimal = false;
+function appendDecimal() {
+    if (decimalCount >= 1) {
+        return;
     } else {
-        previousNumber = currentNumber;
-        operator = op;
-        maxDecimal = false;
-        shouldReset = true;
+        currentNumberDisplay.textContent += '.'
+        decimalCount++;
     }
 }
 
-function operate() {
-    previousNumber = Number(previousNumber);
-    currentNumber = Number(currentNumber);
 
-    if (operator === "+") {
-        return add(previousNumber, currentNumber);
-    } else if (operator === "-") {
-        return subtract(previousNumber, currentNumber);
-    } else if (operator === "*") {
-        return multiply(previousNumber, currentNumber);
-    } else if (operator === "/") {
-        if (currentNumber === 0) {
-            alert("Can't divide by zero! Please try again.");
-            return;
-        } else {
-            return divide(previousNumber, currentNumber);
-        }
-    }
-}
 
-function handleDecimal() {
-    if (!maxDecimal) {
-        currentNumber += '.';
-        currentNumberDisplay.textContent = currentNumber;
-        maxDecimal = true;
-    }
-}
-
-function handleEqual() {
-    currentNumber = operate();
-    currentNumberDisplay.textContent = currentNumber;
-    shouldOperate = false;
-}
-
-function handleKeyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) handleNumber(e.key);
-    if (e.key === '.') handleDecimal();
-    if (e.key === '=' || e.key === 'Enter') handleEqual();
-    if (e.key === 'Backspace') del();
-    if (e.key === 'Escape') clear();
-    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-      handleOperator(e.key);
-  }
 
 function add(a, b) {
     return a + b;
