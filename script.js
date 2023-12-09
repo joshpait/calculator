05dc077411a8e98ed42c08e13944b337efcff2be
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-12-08 18:12:08 -0800

    Fixed js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let firstOp = ''; // current number button pressed
-let secondOp= ''; // previous operation result
-let operator = null; // operators
+let firstOperand = ''; // current number button pressed
+let secondOperand = ''; // previous operation result
+let currentOperator = null; // operators
 let decimalCount = 0;
+let shouldResetScreen = false;
 
 const currentNumberDisplay = document.querySelector('.current-number');
-
 const clearButton = document.querySelector('.clear-button');
 const deleteButton = document.querySelector('.delete-button');
 const numberButtons = document.querySelectorAll('.num');
@@ -13,35 +13,20 @@ const decimalButton = document.querySelector('.decimal-button');
 const equalsButton = document.querySelector('.equals-button');
 
 window.addEventListener('keydown', handleKeyboardInput);
-numberButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        appendNumber(button.textContent);
-    });
-});
+numberButtons.forEach(button => button.addEventListener('click', () => appendNumber(button.textContent)));
+decimalButton.addEventListener('click', () => appendDecimal());
+operatorButtons.forEach(button => button.addEventListener('click', () => setOperation(button.value)));
+clearButton.addEventListener('click', () => clear());
+deleteButton.addEventListener('click', () => del());
+equalsButton.addEventListener('click', () => evaluate());
 
-decimalButton.addEventListener('click', () => {
-    appendDecimal();
-})
-
-operatorButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        handleOperator(button.value);
-    })
-})
-
-clearButton.addEventListener('click', () => {
-    clear();
-})
-
-deleteButton.addEventListener('click', () => {
-    del(currentNumber);
-})
-
-equalsButton.addEventListener('click', () => {
-    handleEqual();
-})
+function resetScreen() {
+    currentNumberDisplay.textContent = '';
+    shouldResetScreen = false;
+}
 
 function appendNumber(number) {
+    if (shouldResetScreen) resetScreen();
     currentNumberDisplay.textContent += number;
 }
 
@@ -54,8 +39,49 @@ function appendDecimal() {
     }
 }
 
+function setOperation(operator) {
+    if (currentOperator !== null) operate();
+    firstOperand = currentNumberDisplay.textContent;
+    currentOperator = operator;
+    shouldResetScreen = true;
+}
 
+function evaluate() {
+    if (currentNumberDisplay.textContent === '0' && currentOperator === '/') {
+        alert("Can't divide by zero!");
+        currentNumberDisplay.textContent = '';
+        return;
+    }
+    secondOperand = currentNumberDisplay.textContent;
+    currentNumberDisplay.textContent = roundResult(operate(firstOperand, secondOperand, currentOperator));
+    currentOperator = null;
+}
 
+function del() {
+    currentNumberDisplay.textContent = currentNumberDisplay.textContent.toString().slice(0, -1);
+}
+
+function clear() {
+    resetScreen();
+    firstOperand = '';
+    secondOperand = '';
+    currentOperator = null;
+    decimalCount = 0;
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === '.') appendDecimal()
+    if (e.key === '=' || e.key === 'Enter') evaluate()
+    if (e.key === 'Backspace') del()
+    if (e.key === 'Escape') clear()
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+      setOperation(e.key)
+}
 
 function add(a, b) {
     return a + b;
@@ -71,4 +97,21 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(a, b, operator) {
+    a = Number(a);
+    b = Number(b);
+
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null;
+            else return divide(a, b);
+    }
 }
